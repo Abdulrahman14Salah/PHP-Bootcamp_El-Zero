73e77ee32c35fc882f45d06a95c7c6fb77aafdaf
Abdulrahman <>
2022-12-31 11:21:47 +0200

    .\assignment-09.php

---
+++ assignment-09.php
@@ -0,0 +1,13 @@
+<?php
+echo nl2br(
+    <<<'break'
+    Hello "'Elzero'"
+    We Love $Programming$
+    Languages Specially "PHP"
+    break
+);
+
+// Needed Output
+// Hello "'Elzero'"
+// We Love $Programming$
+// Languages Specially "PHP"
