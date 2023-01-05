3b93985c3ae94eca9ddac79f57be7c71e81f2457
Abdulrahman <>
2023-01-05 16:36:26 +0200

    Assignment 06

--- Data Types [006] to [12]/assignment-06.php
+++ Data Types [006] to [12]/assignment-06.php
@@ -1,1 +1,13 @@
 <?php
+$something = "Programming";
+
+echo <<<"code"
+Hello \PHP\
+We Love $something
+code . "<br>";
+
+// [1] Fix The Error
+// [2] Remove 2 Characters To Get The Output
+
+// Needed Output
+// Hello \PHP\ We Love Programming
