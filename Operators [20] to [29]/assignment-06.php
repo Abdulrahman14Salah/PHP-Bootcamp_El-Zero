eb3a437dd8d83ea14310085e3b69e84cd7639ffc
Abdulrahman <>
2023-01-06 16:38:28 +0200

    @andulrahman14salah

---
+++ Operators [20] to [29]/assignment-06.php
@@ -0,0 +1,19 @@
+<?php
+
+$a = "Elzero";
+$b = "Web";
+$c = "School";
+
+// Method One
+// $d = $a . " " . $b . " " . $c;
+
+// Method Two
+// $d = "$a $b $c";
+
+// Method Three
+// $d = $a .= " " . $b . " " . $c . " ";
+
+// Method Four
+// $d = ???;
+
+echo $d; // Elzero Web School
---
+++ Operators [20] to [29]/assignment-08.php
@@ -0,0 +1,10 @@
+<?php
+
+// Code 1
+@$a = $b or die("Custom Error Variable");
+
+// Code 2
+$f = @file("Not_A_File") or die("Custom Error File");
+
+// Code 3
+(@include("Not_A_File")) or die("Custom Error Include");
