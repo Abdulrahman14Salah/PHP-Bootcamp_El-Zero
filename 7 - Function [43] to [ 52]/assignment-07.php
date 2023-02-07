b3be6049621ac8cc0e3325f288a0e12f59e741b7
Abdulrahman14Salah <3a.sala7@gmail.com>
2023-02-07 23:49:24 +0200

    @abdulrahman14salah

---
+++ 7 - Function [43] to [ 52]/assignment-07.php
@@ -0,0 +1,12 @@
+<?php
+
+function calculate(int $num_one, int $num_two)
+{
+    $num_one = (float) $num_one;
+    return $num_one + $num_two;
+}
+
+echo '<br>';
+echo calculate(20, 10); // 30
+echo '<br>';
+echo gettype(calculate(20, 10)); // Double
